b734d4b5c592a7dffb902aa6ad0bdbe4ab581614
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-02-12 20:18:29 +0100

    Updates

---
+++ precache-manifest.c1035f010517b45e99817bdb4ec27fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.72d2a57e.js"
+  },
+  {
+    "revision": "830016c1e79d3a89c1fa",
+    "url": "/tic-tac-toe/static/js/main.830016c1.chunk.js"
+  },
+  {
+    "revision": "464e95bb0b06898eeec8",
+    "url": "/tic-tac-toe/static/js/2.464e95bb.chunk.js"
+  },
+  {
+    "revision": "830016c1e79d3a89c1fa",
+    "url": "/tic-tac-toe/static/css/main.47453b4e.chunk.css"
+  },
+  {
+    "revision": "086b0beb014e0113a7b2af0979ac0cfa",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
